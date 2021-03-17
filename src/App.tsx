082aa5946bffb11dcc33217707408473578e8901
Christian Fleschhut <weltrekordhalter@gmail.com>
2021-03-18 00:31:46 +0100

    Fetch products using React Query

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,29 @@ import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { Wrapper } from "./App.styles";
 
+export type CartItemType = {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  createdAt: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  fetch(
+    "https://mockend.com/cfleschhut/react-typescript-shopping-cart/products"
+  ).then((res) => res.json());
+
 const App = () => {
+  const { isLoading, error, data } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
+
   return (
     <div className="App">
       <Drawer></Drawer>
